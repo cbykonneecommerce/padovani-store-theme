5ed97a99e8e5d0488ec32e88a93ab42be356d7d2
unknown <eduardo.soares@acct.global>
2025-06-04 10:00:16 -0300

    feat: dropdown attachment

--- front/react/components/Attachments/AttachmentDropDown.jsx
+++ front/react/components/Attachments/AttachmentDropDown.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useCssHandles } from 'vtex.css-handles'
 import { Dropdown } from 'vtex.styleguide'
 
@@ -6,7 +6,11 @@ const CSS_HANDLES = ['attachmentSelectButton', 'attachmentDropdownContainer']
 
 export default function AttachmentsDropdown({ setSelected, item }) {
   const { handles } = useCssHandles(CSS_HANDLES)
-  const defaultValue = item?.options[0]
+  const defaultValue = {
+    id: 'selecione',
+    label: 'selecione',
+    value: '',
+  }
   const [dropdownValue, setDropdownValue] = useState(defaultValue.value)
   const lowerTitle = item?.name?.toLowerCase()
 
@@ -14,19 +18,28 @@ export default function AttachmentsDropdown({ setSelected, item }) {
     return null
   }
 
-  const newOptions = item?.options?.map((opt) => {
-    return {
+  let newOptions = [
+    {
+      label: 'Escolha uma opção',
+      value: '',
+      disabled: true,
+    },
+  ]
+
+  item?.options?.forEach((opt) => {
+    newOptions.push({
       value: opt.value,
       label: opt.value,
-    }
+    })
   })
-
-  useEffect(() => {
-    setSelected(defaultValue)
-  }, [])
 
   const handleFilter = (event) => {
     const { value } = event.target
+
+    if (value === '') {
+      return
+    }
+
     const selectedData = item?.options?.find((opt) => opt.value === value)
     setDropdownValue(value)
 
